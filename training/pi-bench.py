ddf5541d8058725414adb5bbf1a7c4fde6bf498d
Morris Hansing <morris@hansing.se>
2025-09-18 15:36:07 +0200

    pi test iter2

--- training/pi-bench.py
+++ training/pi-bench.py
@@ -12,7 +12,7 @@ from torch.utils.data import DataLoader
 from unet_model import ResUNetSE
 import os
 
-def benchmark_unet(features, in_channels=1, out_channels=1, img_size=512, device='cpu', N_WARMUPS=10, N_RUNS=100):
+def benchmark_unet(features, in_channels=1, out_channels=1, img_size=512, device='cpu', n_warmups=10, n_runs=100):
     model = ResUNetSE(in_channels=in_channels, out_channels=out_channels, features=features, se_reduction=int(features/4)).to(device)
     model.eval()
     
@@ -30,27 +30,35 @@ def benchmark_unet(features, in_channels=1, out_channels=1, img_size=512, device
 
     # Warm-up
     with torch.no_grad():
-        for _ in range(N_WARMUPS):
+        for _ in range(n_warmups):
             _ = model(x)
     
     # Benchmark
     start_time = time.time()
     with torch.no_grad():
-        for _ in range(N_RUNS):
+        for _ in range(n_runs):
             _ = model(x)
     end_time = time.time()
     
-    avg_time = (end_time - start_time) / N_RUNS
-    print(f"  Avg Inference Time: {avg_time:.6f} seconds. (of {N_RUNS}, with {N_WARMUPS} warm-ups)")
+    avg_time = (end_time - start_time) / n_runs
+    print(f"  Avg Inference Time: {avg_time:.6f} seconds. (of {n_runs}, with {n_warmups} warm-ups)")
     return avg_time
 
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+N_WANRMUPS = 1
+N_RUNS = 5
+
+device = 'cpu'
 print(f"Using device: {device}")
-for features in [16, 32, 64]:
-    benchmark_unet(features, device=device)
-    # sleep to un-warmup
-    time.sleep(5)
+
+if torch.cuda.is_available():
+    print("this benchmark is not made for the GPU, but try it anyway if you want")
+
+for size in [512, 1024]:
+    print(f"\n\nImage size: {size}x{size}\n")
+    for features in [16, 32, 64]:
+        benchmark_unet(features, img_size=size, device=device, n_warmups=N_WANRMUPS, n_runs=N_RUNS)
+        time.sleep(5) # sleep to un-warmup
 
 
 
